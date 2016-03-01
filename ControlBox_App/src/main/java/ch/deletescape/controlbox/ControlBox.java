3b07a5df2a8aaffa4c51e612cd89695694109651
Deletescape <deletescape@outlook.com>
2016-03-01 20:00:14 +0100

    add in app update notifications
    VERY EXPERIMENTAL, APP COULD CRASH

--- ControlBox_App/src/main/java/ch/deletescape/controlbox/ControlBox.java
+++ ControlBox_App/src/main/java/ch/deletescape/controlbox/ControlBox.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+
 public class ControlBox extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_box);
+        new AppUpdater(this.getBaseContext())
+                .setUpdateFrom(UpdateFrom.GITHUB)
+                .setGitHubUserAndRepo("deletescape", "Control_Box_Android")
+                .setDisplay(Display.SNACKBAR)
+                .showAppUpdated(true)
+                .start();
     }
 
     public void startRebooter(View view) {
