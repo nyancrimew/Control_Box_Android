7baf4a070f6d03d1e27954acd7d1542b2dfeaf73
Deletescape <deletescape@outlook.com>
2016-03-03 20:53:17 +0100

    refactoring of AppUpdater stuff

--- ControlBox_App/src/main/java/ch/deletescape/controlbox/ControlBox.java
+++ ControlBox_App/src/main/java/ch/deletescape/controlbox/ControlBox.java
@@ -2,6 +2,7 @@ package ch.deletescape.controlbox;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -15,11 +16,21 @@ public class ControlBox extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_box);
-        new AppUpdater(this.getBaseContext())
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException ignored) {
+        }
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        new AppUpdater(this.getApplicationContext())
                 .setUpdateFrom(UpdateFrom.GITHUB)
                 .setGitHubUserAndRepo("deletescape", "Control_Box_Android")
                 .setDisplay(Display.NOTIFICATION)
-                .showAppUpdated(true)
+                .showAppUpdated(false)
                 .start();
     }
 
